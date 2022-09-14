b8ca7bce6a7b6aecc1bd4629d34bf5fcbfdbb843
Ludwig Ivan Comiso <comisoivan@gmail.com>
2022-09-15 07:41:50 +0800

    removed app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { ProductModule } from './modules/product/product.module';
-import { join } from 'path';
 
 @Module({
   imports: [
@@ -26,7 +24,6 @@ import { join } from 'path';
     }),
     ProductModule,
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
